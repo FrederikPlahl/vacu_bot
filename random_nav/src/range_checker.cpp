0ba42e04f23ede4dbda0ef5ea32b0285e8d0702a
FrederikPlahl <frederik@plahl.eu>
2021-05-29 09:59:13 +0200

    check all ranges

--- random_nav/src/range_checker.cpp
+++ random_nav/src/range_checker.cpp
@@ -14,29 +14,29 @@ void rangeCheck(const sensor_msgs::LaserScan scan_msg)
 
 
   float max_range = 1.0;
-  
+  int len = sizeof(scan_msg.ranges);
 
-  if (scan_msg.ranges[0] <= max_range)
-  {
-    //rückwärts und drehen
-    
-    ROS_INFO("Careful! There is a wall.");
+  for (int i = 0; i < len; i++){
+    if (scan_msg.ranges[i] <= max_range)
+    {
+      //rückwärts und drehen
+      
+      ROS_INFO("Careful! There is a wall.");
 
-    twist_msg.linear.x = -1;
-    twist_msg.angular.z = -0.5;
+      twist_msg.linear.x = -1;
+      twist_msg.angular.z = -0.5;
+    }
+    else
+    {
+      //geradeaus fahren
 
+      ROS_INFO("No wall! I'm going straight.");
+
+      twist_msg.linear.x = 0.3;    
+    }
+
+    pub.publish(twist_msg);
   }
-  else
-  {
-    //geradeaus fahren
-
-    ROS_INFO("No wall! I'm going straight.");
-
-    twist_msg.linear.x = 0.3;    
-  }
-
-  pub.publish(twist_msg);
-
 }
 
 int main(int argc, char **argv)
